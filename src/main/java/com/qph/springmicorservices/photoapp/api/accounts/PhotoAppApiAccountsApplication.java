c715f1d3f0838274f6129a71708fde26429f1066
Quoc PHAN <phquoc25@gmail.com>
2020-05-26 22:18:55 +0700

    Initialize the project

---
+++ src/main/java/com/qph/springmicorservices/photoapp/api/accounts/PhotoAppApiAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.qph.springmicorservices.photoapp.api.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiAccountsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qph/springmicorservices/photoapp/api/accounts/PhotoAppApiAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qph.springmicorservices.photoapp.api.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
